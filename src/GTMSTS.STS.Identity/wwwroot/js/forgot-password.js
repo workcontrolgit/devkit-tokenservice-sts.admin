0e8b874b815fdefeac132c79cbf97662bb1fbdb5
fuji.nguyen <fuji.nguyen@workcontrol.com>
2020-09-04 23:56:39 -0400

    Init version

--- src/GTMSTS.STS.Identity/wwwroot/js/forgot-password.js
+++
@@ -1,9 +0,0 @@
-﻿$(function () {
-    $('input[name="Policy"]').change(function () {
-        var selectedLoginPolicy = $('[name=Policy]:checked').val();
-        $('.resetPasswordBy').hide();
-        $('#resetPasswordBy' + selectedLoginPolicy).show();
-    });
-
-    $('.resetPasswordBy').hide();
-});
